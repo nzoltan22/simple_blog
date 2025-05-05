24babf1f617d2009a450a94d70214e453d09795a
Németh Zoltán <nzoltan2@gmail.com>
2025-05-05 16:10:29 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Egyszerű Személyes Blog Laravel keretrendszerben
+
+## Ez a project használja
+
+- Laravel Breeze 
+- Tailwind CSS
+- Auth Starter Kit.
+
+## Hogyan kell instalálni
+
+- Klónozd a repositoryt a __git clone__ paranccsal.
+- Futtasd a __composer install__ parancsot.
+- Futtasd a __php artisan key:generate__ parancsot.
+- Futtasd a __php artisan migrate --seed__ parancsot (ez tartalmaz némi mag adatot a teszteléshez).
+- Futtasd a __php artisan storage:link__ parancsot (public/storage -> storage/app/public)
+- A cikkek kezeléséhez alapértelmezett bejelentkezés: __admin@admin.com__ - __password__
+
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            Blogomról
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Az egyensúly megtalálása az élet kulcsfontosságú területei között: munka, egészség és szabadidő.
+                    A cikkek arra ösztönöznek, hogy ne csak túlélni próbáljunk a mindennapokban, hanem tudatosan alakítsuk ki azt az életformát, amelyben a munka nemcsak kötelesség, hanem önkifejezés és hivatás; az egészség nem cél, hanem mindennapi törődés önmagunkkal; a szabadidő pedig nem luxus, hanem nélkülözhetetlen feltöltődés és lelki táplálék.
+                    Az írások arra hívnak, hogy újragondoljuk ezekhez a területekhez való viszonyunkat, és közelebb kerüljünk egy harmonikusabb, értékesebb és örömtelibb élethez.
+                </div>
+            </div>
+        </div>
+</x-app-layout>
